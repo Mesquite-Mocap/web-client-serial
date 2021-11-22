b0911c3f00b312e3f65a7238a29af1c0ab882264
ishrat <90342441+ishtotheizzo@users.noreply.github.com>
2021-11-22 11:47:39 -0700

    Update mappings.js
    Updated documentation

--- docs/mappings.js
+++ docs/mappings.js
@@ -1,5 +1,11 @@
+/* Remember the Mac addresses?  This is where you align the Mac addresses to the seed.
+id: these are cases for us to code in custom.js. These are the cases that will align limb, to coordinates to be visualized
+calibration: this is the T pose
+last: last location 
+actual: the real time data stream*/
+
 var mac2Bones = {
-  "84:0D:8E:07:32:24" : {id: "LeftArm", calibration:{x:0, y:0, z:0, w:1}, last:{x:0, y:0, z:0, w:1}, actual:{x:0, y:0, z:0, w:1}},
+  "84:0D:8E:07:32:24" : {id: "LeftArm", calibration:{x:0, y:0, z:0, w:1}, last:{x:0, y:0, z:0, w:1}, actual:{x:0, y:0, z:0, w:1}}, 
   "24:62:AB:DD:C4:3C" : {id: "LeftForeArm", calibration:{x:0, y:0, z:0, w:1}, last:{x:0, y:0, z:0, w:1}, actual:{x:0, y:0, z:0, w:1}},
   "00:00:00:00:00:02" : {id: "LeftHand", calibration:{x:0, y:0, z:0, w:1}, last:{x:0, y:0, z:0, w:1}, actual:{x:0, y:0, z:0, w:1}},
   "00:00:00:00:00:03" : {id: "LeftUpLeg", calibration:{x:0, y:0, z:0, w:1}, last:{x:0, y:0, z:0, w:1}, actual:{x:0, y:0, z:0, w:1}},
